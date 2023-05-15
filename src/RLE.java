f1505c800c5fb14d1dcfb2a0ba1f0fffc7fd24f5
Pau <pauramisnunez@gmail.com>
2023-05-15 16:14:13 +0200

    Initial commit

---
+++ src/RLE.java
@@ -0,0 +1,12 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RLE {
+    public static void compress(InputStream is, OutputStream os) {
+
+    }
+
+    public static void decompress(InputStream is, OutputStream os) {
+
+    }
+}
