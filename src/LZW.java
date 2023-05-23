4d6115d78189b1512c916a9185bba941c39dcce3
Your Name <you@example.com>
2023-05-23 19:17:18 +0200

    Starting LZW

--- src/LZW.java
+++ src/LZW.java
@@ -1,10 +1,23 @@
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LZW {
-    public static void compress(InputStream is, OutputStream os) {
+    public static void compress(InputStream is, OutputStream os) throws IOException {
+        int isLength = is.available();
+        int current;
+        int last;
+        List<Integer> dictionary = new ArrayList<>();
 
+        for (int i = 0; i < isLength; i++) {
+            current = is.read();
+            if (!dictionary.contains(current))
+                dictionary.add(current);
+        }
+        //os.write(current);
     }
 
     public static void decompress(InputStream is, OutputStream os) {
